8ef5694f0db683101d91cfc1259955a4ca30ee4d
Christian Sanchez <cas81695@gmail.com>
2020-05-06 02:15:47 -0400

    added folders in src, with files on the main page and data area

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataArea from "./DataArea";
+
+
+function Main () {
+    return (
+      <>
+        <DataArea />
+      </>
+    );
+  };
+
+export default Main;
