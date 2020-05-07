66b7e2ca2b11510ba8f8b9626553aa02ef63e8d5
Christian Sanchez <cas81695@gmail.com>
2020-05-06 21:40:54 -0400

    added nav and header files

---
+++ src/components/Nav.js
@@ -0,0 +1,28 @@
+import React from "react";
+import SearchName from "./SearchName.js";
+import SearchDOB from "./SearchDOB.js";
+import "../styles/Nav.css";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse row" id="navbarNav">
+        <div className="search-area col-4">
+          <SearchName />
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default Nav;
