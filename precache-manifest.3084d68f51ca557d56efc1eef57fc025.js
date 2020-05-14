550fa638e73a24f6dbf59f68b6da7404b9e16098
Christian Sanchez <cas81695@gmail.com>
2020-05-14 16:44:45 -0400

    Updates

---
+++ precache-manifest.3084d68f51ca557d56efc1eef57fc025.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072f0e28113f2663a7d2cf980100042d",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "043000d77690faa52de6",
+    "url": "/Employee-Directory/static/css/main.a132ece4.chunk.css"
+  },
+  {
+    "revision": "500279d0ef3d12409314",
+    "url": "/Employee-Directory/static/js/2.b65be91f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.b65be91f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043000d77690faa52de6",
+    "url": "/Employee-Directory/static/js/main.82f6ad13.chunk.js"
+  },
+  {
+    "revision": "2576aae6e46e89f2547d",
+    "url": "/Employee-Directory/static/js/runtime-main.bea4382b.js"
+  }
+]);
